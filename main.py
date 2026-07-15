f99b053c25e2b84a09f55c851d827ec4eb85c535
adrita50 <adrita.das2020@gmail.com>
2026-07-15 21:53:24 +0600

    conversation chain

--- main.py
+++ main.py
@@ -4,25 +4,26 @@ from pypdf import PdfReader
 from langchain_community.document_loaders import WebBaseLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from dotenv import load_dotenv
-from langchain_cohere import CohereEmbeddings
+from langchain_cohere import CohereEmbeddings,ChatCohere
+from langchain_classic.chains import ConversationalRetrievalChain
 from langchain_core.documents import Document
 from langchain_qdrant import QdrantVectorStore
 from qdrant_client import QdrantClient
 
 
-load_dotenv()
 
-def get_document(user_input):
+load_dotenv()
+COHERE_API_KEY = os.getenv('COHERE_API_KEY')
+
+# read from pdf 
+
+def get_document(target_pdf):
     
     document = []
 
-    # text = ''
-    
-    # read pdf
-    if user_input and user_input.files:
-        pdf = user_input.files
-        for pdf_files in pdf:
-            read_pdf = PdfReader(pdf_files)
+    if target_pdf:
+        for pdf in target_pdf:
+            read_pdf = PdfReader(pdf)
             
             for page_num,page in enumerate(read_pdf.pages, start=1):
                 page_text = page.extract_text()
@@ -30,71 +31,128 @@ def get_document(user_input):
                 document.append(Document(page_content = page_text , metadata= {"page" : page_num }
                     
                  ))  
-                
-     # read from url 
-    
-    if user_input and user_input.text:
-        read_url = WebBaseLoader(user_input.text)
-        url = read_url.load()
-        document.extend(url)
-                        
-    
+            
     return document
 
+# read from url
+
+def get_url(target_url):
+    
+    url = []
+    
+    if target_url:
+        try:
+            read_url = WebBaseLoader(web_paths=[target_url])
+            url.extend(read_url.load())
+            
+        except Exception:
+            pass
+             
+    return url    
+
+# create chunk
+    
 def get_chunk(chunk):
     
-    text_spliter = RecursiveCharacterTextSplitter(
-        
-        chunk_size = 1000,
-        chunk_overlap = 200,
-
-    )
+    text_spliter = RecursiveCharacterTextSplitter(chunk_size = 1000,chunk_overlap = 200)
     
     chunks = text_spliter.split_documents(chunk)
     
     return chunks
 
+# embeddings & vector database 
+
 def get_embeddigs(chunk_text):
     embeddings = CohereEmbeddings(
         model='embed-english-v3.0',
-        cohere_api_key=os.getenv('COHERE_API_KEY')
+         cohere_api_key=os.getenv('COHERE_API_KEY')
     )
     
+        
     vector_db = QdrantVectorStore.from_documents(
         documents = chunk_text,
         embedding= embeddings,
-        path= 'qdrant_storage',
+        location=':memory:',
+        #path= 'qdrant_storage',
         collection_name='rag_collection',
-        
+        force_recreate=True
+            
     )
     
     return vector_db
-    
 
+
+def user_query(vector_store):
+    llm = ChatCohere(
+        
+        cohere_api_key = os.getenv('COHERE_API_KEY'),
+        model = 'command-r-08-2024', 
+        temperature = 0.7,
+        max_tokens = 512
+        
+    )
+    
+    conversation_chain = ConversationalRetrievalChain.from_llm(
+        llm = llm,
+        retriever=vector_store.as_retriever()
+    )
+    
+    return conversation_chain
+      
 def main():
     st.set_page_config(page_title='Chat with URL & PDF', page_icon=':books:')
     
-    user_input = st.chat_input('Ask a question', accept_file='multiple', file_type='pdf')
     
-    raw_text = get_document(user_input)
-    
-    # with st.sidebar:
-    #     raw_text = get_document(user_input)
+    if 'conversation' not in st.session_state:
+        st.session_state.conversation = []
         
-    #     if raw_text:
-    #         # st.write(raw_text)
-    #         chunk_text = get_chunk(raw_text)
+    if 'conversation_chain' not in st.session_state:
+        st.session_state.conversation_chain = None
+        
+    for role,message in st.session_state.conversation:
+        with st.chat_message(role):
+            st.write(message)
+
+                        
+    user_input = st.chat_input('Ask a question', accept_file='multiple', file_type='pdf' , key='url')
+    
+    if user_input:
+        pdf_files = user_input.files      
+        text_input = user_input.text
+        
+        raw_text = get_document(pdf_files)
+        
+        if raw_text:
+             chunk_text = get_chunk(raw_text)
+             vectorStore = get_embeddigs(chunk_text)
+             st.session_state.vector_store = vectorStore
             
-    #         if chunk_text:
-    #              st.write(chunk_text)
-    
-    if raw_text:
-        chunk_text = get_chunk(raw_text)
-        # return chunk_text
+
+        raw_text_url = get_url(text_input)  
         
-    vectorStore = get_embeddigs(chunk_text)    
+        if raw_text_url:
+            chunk_text_url = get_chunk(raw_text_url)
+            vectorStoreUrl =get_embeddigs(chunk_text_url)
+            st.session_state.vector_store = vectorStoreUrl  
         
-    
+        if text_input:
+            st.session_state.conversation.append(('user' , text_input))
+            with st.chat_message('user'):
+                st.markdown(text_input)
+                
+            if 'vector_store' in st.session_state:
+                chain = user_query(st.session_state.vector_store)
+                
+                with st.chat_message('assistant'):
+                    with st.spinner('Thinking....'):
+                        response = chain.invoke({'question' : text_input , 'chat_history' : []})
+                        answer = response['answer']
+                        st.markdown(answer)
+                        
+                st.session_state.conversation.append(('assistant' , answer))
+            
+        else:
+            st.warning('Please upload a PDF or URL first')                      
            
 if __name__ == '__main__':
     main()  
